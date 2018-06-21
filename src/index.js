807d8a36c801971af03a4b440fa9eba6d79bf264
vanillafrosty <dwu.inq@gmail.com>
2018-06-21 02:41:19 -0400

    add new Link type and feed root field to schema and resolvers

--- src/index.js
+++ src/index.js
@@ -3,14 +3,34 @@ const { GraphQLServer } = require('graphql-yoga')
 // 1
 const typeDefs = `
 type Query {
-  info: String!
+  info: String!,
+  feed: [Link!]!
+}
+
+type Link {
+  id: ID!,
+  description: String!,
+  url: String!
 }
 `
+
+let links = [{
+  id: 'link-0',
+  description: 'Hello world i am link 0',
+  url: 'link/0'
+}]
 
 // 2
 const resolvers = {
   Query: {
-    info: () => 'Hello World!'
+    info: () => 'Hello World!',
+    feed: () => links
+  },
+
+  Link: {
+    id: (root) => root.id,
+    description: (root) => root.description,
+    url: (root) => root.url,
   }
 }
 
