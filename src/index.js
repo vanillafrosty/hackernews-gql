52ed48f988ac27075db42e75ac3ad9cc5c463006
vanillafrosty <dwu.inq@gmail.com>
2018-06-21 10:49:28 -0400

    begin full CRUD schema defs

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ let links = [{
   url: 'link/0'
 }]
 
+let idCount = links.length;
 
 const resolvers = {
   Query: {
@@ -15,12 +16,29 @@ const resolvers = {
     feed: () => links
   },
 
-  Link: {
-    id: (root) => root.id,
-    description: (root) => root.description,
-    url: (root) => root.url,
+  Mutation: {
+    post: (root, args) => {
+      const link = {
+        url: args.url,
+        description: args.description,
+        id: `link-${idCount++}`
+      }
+      links.push(link)
+      return link
+    }
   }
+
 }
+
+//the below isn't necessary in our resolvers, because graphQL is
+//smart enough to assume this for links (specifically our feed query).
+//general NOTE: (root) in this case represents each Link returned by the
+//feed query. so each of the three resolvers below are called per Link.
+// Link: {
+//   id: (root) => root.id,
+//   description: (root) => root.description,
+//   url: (root) => root.url,
+// }
 
 const server = new GraphQLServer({
   typeDefs: './src/schema.graphql',
