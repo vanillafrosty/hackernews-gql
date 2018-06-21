3502930322979914e39157cb56da89e40f053f76
vanillafrosty <dwu.inq@gmail.com>
2018-06-21 09:39:53 -0400

    refactor typedefs into their own file, add new Link mutation

--- src/index.js
+++ src/index.js
@@ -1,18 +1,6 @@
 const { GraphQLServer } = require('graphql-yoga')
 
-// 1
-const typeDefs = `
-type Query {
-  info: String!,
-  feed: [Link!]!
-}
 
-type Link {
-  id: ID!,
-  description: String!,
-  url: String!
-}
-`
 
 let links = [{
   id: 'link-0',
@@ -20,7 +8,7 @@ let links = [{
   url: 'link/0'
 }]
 
-// 2
+
 const resolvers = {
   Query: {
     info: () => 'Hello World!',
@@ -34,9 +22,8 @@ const resolvers = {
   }
 }
 
-// 3
 const server = new GraphQLServer({
-  typeDefs,
+  typeDefs: './src/schema.graphql',
   resolvers,
 })
 server.start(() => console.log(`Server is running on http://localhost:4000`))
